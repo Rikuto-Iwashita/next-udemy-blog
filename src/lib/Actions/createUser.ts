e4154ad7e2f9f3267b83d3f5513bb5ad7970744c
Rikuto Iwashita <02rikuto18@gmail.com>
2025-07-02 08:25:57 +0900

    76:ユーザー登録フォーム。エラーも解決できました

--- src/lib/Actions/createUser.ts
+++ src/lib/Actions/createUser.ts
@@ -1,59 +1,56 @@
 'use server'
 
 import { registerSchema } from "@/validations/user"
-import { prisma } from '@/lib/prisma'
+import { prisma } from "@/lib/prisma"
 import bcryptjs from 'bcryptjs'
 import { signIn } from "@/auth"
-import { redirect } from 'next/navigation'
-import { ZodError } from 'zod'
+import { redirect } from 'next/navigation';
 
 type ActionState = {
-    success: boolean,
+    success: boolean;
     errors: Record<string, string[]>
 }
 
 // バリデーションエラー処理
-function handleValidationError(error: ZodError): ActionState {
-    const { fieldErrors, formErrors } = error.flatten();
-    const castedFieldErrors = fieldErrors as Record<string, string[]>;
-    // zodの仕様でパスワード一致確認のエラーは formErrorsで渡ってくる
-  // formErrorsがある場合は、confirmPasswordフィールドにエラーを追加
-    if (formErrors.length > 0) {
-      return { success: false, errors: { ...fieldErrors, confirmPassword: formErrors
-        }}}
-    return { success: false, errors: castedFieldErrors };
-  }
-  
-  // カスタムエラー処理
-  function handleError(customErrors: Record<string, string[]>): ActionState {
-    return { success: false, errors: customErrors };
-  }
+function handleValidationError(error: any): ActionState {
+const { fieldErrors, formErrors } = error.flatten();
+// zodの仕様でパスワード一致確認のエラーは formErrorsで渡ってくる
+// formErrorsがある場合は、confirmPasswordフィールドにエラーを追加
+if (formErrors.length > 0) {
+return { success: false, errors: { ...fieldErrors, confirmPassword: formErrors
+}}}
+return { success: false, errors: fieldErrors };
+}
+// カスタムエラー処理
+function handleError(customErrors: Record<string, string[]>): ActionState {
+return { success: false, errors: customErrors };
+} 
 
 export async function createUser(
     prevState: ActionState,
     formData: FormData
 ): Promise<ActionState>
 {
-    // フォームから渡ってきた情報を取得
+    //フォームからわたってきた情報を取得
     const rawFormData = Object.fromEntries(
-        ["name", "email", "password", "confirmPassword"].map(field =>[
+        ["name", "email", "password", "confirmPassword"].map(field => [
             field,
             formData.get(field) as string
         ])
     ) as Record<string, string>
 
-    // バリデーション
+    //バリデーション
     const validationResult = registerSchema.safeParse(rawFormData)
-    if(!validationResult.success){
+    if (!validationResult.success) {
         return handleValidationError(validationResult.error)
     }
 
-    // DBにメールアドレスが存在しているかの確認
+    // DBにメールアドレスが存在しているのかの確認
     const existingUser = await prisma.user.findUnique({
-        where : { email: rawFormData.email }
+        where : {email : rawFormData.email}
     })
-    if(existingUser){
-        return handleError({email: ['このメールアドレスはすでに登録されています。']})
+    if (existingUser){
+        return handleError({email: ['このメールアドレスは既に登録されています']})
     }
 
     // DBに登録
@@ -69,8 +66,8 @@ export async function createUser(
 
     // dashboardにリダイレクト
     await signIn('credentials', {
-      ...Object.fromEntries(formData),
-      redirect: false
+        ...Object.fromEntries(formData),
+        redirect: false
     })
 
     redirect('/dashboard')
